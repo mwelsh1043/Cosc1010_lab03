3db44fb4c3272144ca85fdd1db089a0b72e60b7b
mwelsh1043 <159499014+mwelsh1043@users.noreply.github.com>
2024-02-13 11:07:37 -0700

    Add files via upload

---
+++ cosc1010-lab03.py
@@ -0,0 +1,62 @@
+# Your Name Here
+# UWYO COSC 1010
+# Submission Date
+# Lab 03 
+# Lab Section: 
+# Sources, people worked with, help given to: 
+# your
+# comments
+# here
+
+
+
+# This is your second lab section. It will primarily be based on the Introducing Lists lecture, reference it if you need
+# Complete all sections of this assignment 
+
+#Part One [10pts] 2pts each section 
+print("Part One------------------------------------------------------------------------")
+#We are going to start with the basics. Declare a list  states that contains the elements: Wyoming, Colorado, Montana in that order 
+#Note this is the ONLY point where you need to declare the states list
+
+
+
+#print the entire list
+
+
+#now print the first element in the list
+
+
+#Print the last element using the syntax shown in class to access the final element (hint, think negatives)
+
+
+#Using an F-string to access the first and second element print the string "COLORADO is south of WYOMING", matching the casing provided
+
+
+
+#Part Two [10pts] 2pts each
+print("Part Two------------------------------------------------------------------------")
+#Append the following states to your list: Washington, Oregon, California and print your list
+
+
+#Again using the specific syntax mentioned in class overwrite the second to last element to be Maine, printing the list 
+
+
+#Insert the state Texas to be the third element in the list, again printing your list
+
+
+#Using the `del` statement remove the fourth item from the list, print your list 
+
+
+#Remove Texas using its value, print the list
+
+
+#Part Three [10pts] 3pts each,  that means a free point built in!
+print("Part Three----------------------------------------------------------------------")
+#Temporarily sort your list, print it both sorted and unsorted 
+
+
+#Permanently sort your list in reverse order, printing it out
+
+
+#Using the reverse method reverse the list and print it
+
